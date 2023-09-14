1cf5d21b6bdcc2cd61384c75f91103d829f7ff84
Michael Vargas Loza <51541540+michaelvargasloza@users.noreply.github.com>
2023-09-13 21:41:35 -0400

    Version 1
    Carga de archivos

---
+++ calc.rb
@@ -0,0 +1,13 @@
+# Solicitar el primer valor
+print "Ingresa el primer valor: "
+valor1 = gets.chomp.to_i
+
+# Solicitar el segundo valor
+print "Ingresa el segundo valor: "
+valor2 = gets.chomp.to_i
+
+# Calcular la suma
+suma = valor1 + valor2
+
+# Mostrar el resultado
+puts "La suma de #{valor1} y #{valor2} es igual a #{suma}"
